511f1ebe3864c0a1f2643164865ef432cc83ce14
price <lixingyu_price@163.com>
2021-01-06 21:07:51 +0800

    Initial Commit

---
+++ PTA&PAT_note&code/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PTA&PAT_note&code
+//
+//  Created by Groveking Li on 2021/1/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
